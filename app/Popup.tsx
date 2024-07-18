c802aa44cdeca12fb2f7dabfbdd2bcf187b1e60e
preitinger <peter.reitinger@gmail.com>
2024-07-19 01:33:28 +0200

    deal correctly with dialog closed by ESC; WhatsApp link half-sized

--- app/Popup.tsx
+++ app/Popup.tsx
@@ -22,7 +22,7 @@ export function Popup({ visible, setVisible, children }: PropsWithChildren<Popup
         <>
             {
                 visible &&
-                <dialog ref={dialogRef} className='dialog'>
+                <dialog ref={dialogRef} className='dialog' onClose={() => { if (setVisible) setVisible(false) }}>
                     <div className={styles.popupOuter}>
                         {
                             setVisible != null &&
--- app/_lib/WhatsAppLinkComp.tsx
+++ app/_lib/WhatsAppLinkComp.tsx
@@ -6,6 +6,6 @@ export interface WhatsAppLinkProps {
 }
 export default function WhatsAppLinkComp({phoneNr}: WhatsAppLinkProps) {
     return (
-        <a href={whatsappLink(phoneNr, '')} target="blank"><Image src='/WhatsApp/Print_Stacked_Green.png' alt='WhatsApp' width={100} height={64}/></a>
+        <a href={whatsappLink(phoneNr, '')} target="blank"><Image src='/WhatsApp/Print_Stacked_Green.png' alt='WhatsApp' width={50} height={32}/></a>
     )
 }
