8c4bd6009b76bae48d6acd2f44b70aefba669d53
preitinger <peter.reitinger@gmail.com>
2024-08-16 04:40:02 +0200

    versuch 2

--- app/_lib/Menu.tsx
+++ app/_lib/Menu.tsx
@@ -144,7 +144,7 @@ export default function Menu({ group, onDeleteMemberClick, customItems, customSp
                         </Popup>
                         <Popup visible={about} setVisible={setAbout}>
                             <h1>About pr-groups</h1>
-                            <p>1</p>
+                            <p>2</p>
                             <Label>Layout & Design der Abstimmungsseite:</Label>
                             <p>ALEXANDER POHL</p>
                             <Label>Layout & Design der Admin-Seiten und Programmierung:</Label>
